a1abe654af74dcf8e6a3368be6f8aed084dbbdd0
DanielDeSousaDEV <danieldesousa.dev@gmail.com>
2024-10-17 14:42:27 -0300

    estudos sobre Blobs

--- Blob/main.js
+++ Blob/main.js
@@ -17,29 +17,45 @@ console.log(Blob)
 
 let myReader = new FileReader()
 
-myReader.addEventListener("loadend",function (evento){
+//Adiciona um ouvinte ao evento de fim de leitura do myReader
+myReader.addEventListener("load", function (evento){
     console.log("evento de leitura")
     console.log(evento)
+
+    console.log("Resultado da leitura")
+    console.log(this.result)
 
     var url = URL.createObjectURL(myBlob)
     
     console.log("URL referente ao Blob")
     console.log(url)
-
 })
+
+//Adiciona um ouvinte ao evento de fim de leitura do myReader
+myReader.onload = function () {
+    alert("acabou uma leitura")
+}
 
 console.log("Instancia do leitor")
 console.log(myReader)
 
 console.log("Pedido de leitura")
+/**
+ * o result do fileReader vai depender do metodo que for usado aqui
+ */
 myReader.readAsArrayBuffer(myBlob)
 
 console.log("status da leitura")
 console.log(myReader.readyState)
 
+setTimeout(() => {
+    console.log('inicio de outra leitura')
+    myReader.readAsArrayBuffer(myBlob)
+}, 5000)
+
 console.log("=====formulario=====");
 
-// let form = document.getElementById('form')
+let form = document.getElementById('form')
 let fileInput = document.getElementById('fileInput')
 
 form.addEventListener('submit', (submitEvent)=>{
